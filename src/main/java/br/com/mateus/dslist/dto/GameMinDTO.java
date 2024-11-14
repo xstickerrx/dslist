b70996ed6305530657f2dbcb09ca1638b0eecb49
xstickerrx <mateus.santana@gmail.com>
2024-11-14 13:04:00 -0300

    get list by id

--- src/main/java/br/com/mateus/dslist/dto/GameMinDTO.java
+++ src/main/java/br/com/mateus/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package br.com.mateus.dslist.dto;
 
 import br.com.mateus.dslist.entities.Game;
+import br.com.mateus.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 	
@@ -19,6 +20,14 @@ public class GameMinDTO {
 		year = entity.getYear();
 		imgUrl = entity.getImgUrl();
 		shortDescription = entity.getShortDescription();
+	}
+	
+	public GameMinDTO(GameMinProjection projection) {
+		id = projection.getId();
+		title = projection.getTitle();
+		year = projection.getYear();
+		imgUrl = projection.getImgUrl();
+		shortDescription = projection.getShortDescription();
 	}
 
 	public Long getId() {
--- src/main/java/br/com/mateus/dslist/services/GameService.java
+++ src/main/java/br/com/mateus/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import br.com.mateus.dslist.dto.GameDTO;
 import br.com.mateus.dslist.dto.GameMinDTO;
 import br.com.mateus.dslist.entities.Game;
+import br.com.mateus.dslist.projections.GameMinProjection;
 import br.com.mateus.dslist.repositories.GameRepository;
 
 
@@ -28,6 +29,13 @@ public class GameService {
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
+		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+		return dto;
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId){
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
 		return dto;
 	}
